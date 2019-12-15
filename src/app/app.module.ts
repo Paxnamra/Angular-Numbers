b01eee7b9a76370ad2edbf9b2443755cebfa3920
Paxnamra <bialyfeniks4@gmail.com>
2019-12-15 15:48:02 +0100

    components structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EvenComponent } from './game-control/even/even.component';
+import { OddComponent } from './game-control/odd/odd.component';
+import { GameControlComponent } from './game-control/game-control.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EvenComponent,
+    OddComponent,
+    GameControlComponent
   ],
   imports: [
     BrowserModule
